a839304114dd6415933c9a723e7d234ec056ce6f
kobeld <kobeld@163.com>
2013-12-13 16:15:23 +0800

    Add more info for debug mode

--- log.go
+++ log.go
@@ -1,7 +1,7 @@
 package mangolog
 
 import (
-	"github.com/paulbellamy/mango"
+	. "github.com/paulbellamy/mango"
 	"log"
 	"time"
 )
@@ -9,12 +9,31 @@ import (
 // Started GET "/look/women" for 111.86.201.25 at Sun Apr 22 11:21:28 +0000 2012
 // Completed 200 OK in 932ms (Views: 738.4ms | ActiveRecord: 183.9ms)
 
-func Logger(env mango.Env, app mango.App) (status mango.Status, headers mango.Headers, body mango.Body) {
-	startTime := time.Now()
-	r := env.Request()
-	log.Printf("Started %s \"%s\" for %s at %s\n", r.Method, r.RequestURI, r.RemoteAddr, startTime.String())
-	status, headers, body = app(env)
-	execution := time.Now().Sub(startTime) / time.Millisecond
-	log.Printf("Completed %d in %dms\n\n", status, execution)
-	return
+func MakeangLogger(debug bool) Middleware {
+	return func(env Env, app App) (status Status, headers Headers, body Body) {
+
+		startTime := time.Now()
+		r := env.Request()
+		if err := r.ParseMultipartForm(32 << 20); err != nil {
+			r.ParseForm()
+		}
+
+		log.Printf("Started %s \"%s\" for %s at %s\n", r.Method, r.RequestURI, r.RemoteAddr, startTime.String())
+
+		if id, ok := r.Form["Id"]; ok {
+			log.Printf("The Id is: %s \n", id)
+		}
+
+		// Log post form value when enabling the debug mode.
+		if debug {
+			log.Printf(" ##### \n Form %v; \n Accept: %+v \n\n", r.Form, r.Header["Accept"])
+		}
+
+		status, headers, body = app(env)
+
+		execution := time.Now().Sub(startTime) / time.Millisecond
+		log.Printf("Completed \"%s\" %d in %dms\n\n", r.RequestURI, status, execution)
+
+		return
+	}
 }
